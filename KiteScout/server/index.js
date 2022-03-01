afa5b82d04a03708eb385cee6dd2d8f388f003be
Karl Hagström <karl.hagstrom@lm.se>
2022-03-01 13:11:50 +0100

    test

--- KiteScout/server/index.js
+++ KiteScout/server/index.js
@@ -13,7 +13,6 @@ function create(env, ctx) {
 
     // Priority serve any static files.
     app.use(express.static(path.resolve(__dirname, '../frontend/build')));
-    //app.use(express.static('~/KiteScout/frontend/build'));
 
     //this is for debugging  
     app.use(function (req, res, next) {
@@ -117,15 +116,13 @@ function create(env, ctx) {
         res.send(JSON.stringify(entries));
     });
 
-    //All remaining requests return the React app, so it can handle routing.
+    // All remaining requests return the React app, so it can handle routing.
     app.get('*', function (request, response) {
-        response.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'), function (err) {
-            if (err) {
-                next(err);
-            } else {
-                console.log('File sent!');
-            }
-        });
+        if (isDev) {
+            response.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+        }else{
+            response.sendFile('~/KiteScout/frontend/build/index.html');
+        }
     });
 
     app.listen(PORT, function () {
